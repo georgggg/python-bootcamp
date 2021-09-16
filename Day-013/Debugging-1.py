600e172790e5a27bd6ebc53884509d7a25784505
Giorgggg <giorgggg.napone@gmail.com>
2021-09-15 21:16:41 -0500

    Adding Day 013 Exercises

---
+++ Day-013/Debugging-1.py
@@ -0,0 +1,19 @@
+# Debug Odd or Even
+# Instructions
+# Read this the code in main.py
+# Spot the problems 🐞.
+# Modify the code to fix the program.
+# Fix the code so that it works and passes the tests when you submit.
+
+# Hint
+# Review the previous lesson and go through the 10 steps to tackle these debugging problems.
+# Solution
+# https://repl.it/@appbrewery/day-13-1-solution
+
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
